8119cb5bccf0be23e93b773b339073bb92b6dfa9
Orians, Jeremiah (DTMB) <OriansJ@michigan.gov>
2022-04-22 09:27:39 -0400

    Add project files.

---
+++ Boom.cs
@@ -0,0 +1,72 @@
+﻿using sqlite_Example;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Person_Generator
+{
+    class Boom
+    {
+        static public Task Generate(string program, string database, string release, string from, string tool, string assignee, List<string> extra_cc_people, List<string> extra_bcc_people, bool slow_mode, string sig)
+        {
+            
+            string cmd = string.Format("--from {0} --tool {1} --release {2} --database {3} --assignee {4} --verbose", from, tool, release, database, assignee);
+            foreach (string s in extra_cc_people)
+            {
+                cmd = cmd + " --cc " + s;
+            }
+            foreach (string s in extra_bcc_people)
+            {
+                cmd = cmd + " --bcc " + s;
+            }
+            if(null != sig)
+            {
+                cmd = cmd + " --sig-file " + sig;
+            }
+            return Task.Run(() => Launch(program, cmd, slow_mode));
+        }
+
+        static private void Launch(string program, string cmd, bool slow_mode)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = program;
+            proc.StartInfo.Arguments = cmd;
+            proc.StartInfo.UseShellExecute = !slow_mode;
+            proc.StartInfo.RedirectStandardOutput = false;
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        static public void fire(string program, string database, string release, string from, string tool, List<string> extra_cc_people, List<string> extra_bcc_people, bool slow_mode, string sig)
+        {
+            SQLiteDatabase mydatabase = new SQLiteDatabase(database);
+
+            try
+            {
+                DataTable ds = mydatabase.GetDataTable("SELECT DISTINCT Username FROM Agile_Teams");
+                List<Task> running = new List<Task>();
+                foreach (DataRow dr in ds.Rows)
+                {
+                    string s = dr["Username"].ToString();
+                    running.Add(Generate(program, database, release, from, tool, s, extra_cc_people, extra_bcc_people, slow_mode, sig));
+                }
+
+                foreach(Task t in running)
+                {
+                    t.Wait();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Environment.Exit(4);
+            }
+
+
+
+            mydatabase.SQLiteDatabase_Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Person_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Person_Generator");
+            string release = null;
+            string tool_generate = null;
+            string tool_send = null;
+            string sender = null;
+            bool fire_email = false;
+            string Database = "example.db";
+            bool debug = false;
+            string signature = null;
+            List<string> message_cc = new List<string>();
+            List<string> message_bcc = new List<string>();
+            int i = 0;
+            while (i < args.Length)
+            {
+                if (match("--release", args[i]))
+                {
+                    release = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--tool-generate", args[i]))
+                {
+                    tool_generate = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--tool-send", args[i]))
+                {
+                    tool_send = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--from", args[i]))
+                {
+                    sender = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--sig-file", args[i]))
+                {
+                    signature = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--database", args[i]))
+                {
+                    Database = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--cc", args[i]))
+                {
+                    message_cc.Add(args[i + 1]);
+                    i = i + 2;
+                }
+                else if (match("--bcc", args[i]))
+                {
+                    message_bcc.Add(args[i + 1]);
+                    if (!fire_email) Console.WriteLine("Warning bcc does not survive writing to a file");
+                    i = i + 2;
+                }
+                else if (match("--fire-email", args[i]))
+                {
+                    fire_email = true;
+                    i = i + 1;
+                }
+                else if (match("--debug-mode", args[i]))
+                {
+                    debug = true;
+                    i = i + 1;
+                }
+                else if (match("--verbose", args[i]))
+                {
+                    int index = 0;
+                    foreach (string s in args)
+                    {
+                        Console.WriteLine(string.Format("argument {0}: {1}", index, s));
+                        index = index + 1;
+                    }
+                    i = i + 1;
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("Unknown argument: {0} received", args[i]));
+                    i = i + 1;
+                }
+            }
+
+            if(null == release)
+            {
+                Console.WriteLine("You need to provide --release YY.MM.DD");
+                Environment.Exit(1);
+            }
+
+            if (null == tool_generate)
+            {
+                Console.WriteLine("You need to provide what tool will generate the messages --tool-generate example.exe");
+                Environment.Exit(2);
+            }
+
+            if (null == tool_send)
+            {
+                Console.WriteLine("You need to provide what tool will send/write the messages to outlook files --tool-send example.exe");
+                Environment.Exit(3);
+            }
+
+            if (null == sender)
+            {
+                Console.WriteLine("You need to provide who you are --from me@email.domain");
+                Environment.Exit(1);
+            }
+
+            Boom.fire(tool_generate, Database, release, sender, tool_send, message_cc, message_bcc, debug, signature);
+            Console.WriteLine("Generation done");
+        }
+
+        static bool match(string a, string b)
+        {
+            return a.Equals(b, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
